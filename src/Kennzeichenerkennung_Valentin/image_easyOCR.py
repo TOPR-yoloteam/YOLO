231b5a3f2ddb5ab587a51ecbb959ab7311a8979c
Valentin.Talmon <talmva01@thu.de>
2025-03-26 16:57:57 +0100

    easyOCR tests

--- src/Kennzeichenerkennung_Valentin/image_easyOCR.py
+++ src/Kennzeichenerkennung_Valentin/image_easyOCR.py
@@ -4,14 +4,17 @@ import os
 
 os.chdir("C:/Users/Valentin.Talmon/PycharmProjects/YOLO/")
 
-image = cv2.imread('src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/licence_plate.png')
+image = cv2.imread('src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/chinese_licence_plate.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-os.chdir("src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/output")
-cv2.imwrite('indian_licence_plate.png', thresh)
 
-reader = easyocr.Reader(['en']) # specify the language
+os.chdir("src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/output")
+
+cv2.imwrite('chinese_licence_plate.png', thresh)
+
+
+reader = easyocr.Reader(['en','ko']) # specify the language
 result = reader.readtext(thresh)
 
 
