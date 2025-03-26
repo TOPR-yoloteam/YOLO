4ccfd9f64d102faaa7468fd41c1bf1e0dbcd4298
Valentin.Talmon <talmva01@thu.de>
2025-03-26 16:22:21 +0100

    easyOCR image anpassung

--- src/Kennzeichenerkennung_Valentin/image_easyOCR.py
+++ src/Kennzeichenerkennung_Valentin/image_easyOCR.py
@@ -4,15 +4,15 @@ import os
 
 os.chdir("C:/Users/Valentin.Talmon/PycharmProjects/YOLO/")
 
-image = cv2.imread('C:/Users/Valentin.Talmon/PycharmProjects/YOLO/src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/img.png')
+image = cv2.imread('src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/licence_plate.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-cv2.imshow('thresh', thresh)
-
+os.chdir("src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/output")
+cv2.imwrite('indian_licence_plate.png', thresh)
 
 reader = easyocr.Reader(['en']) # specify the language
-result = reader.readtext('C:/Users/Valentin.Talmon/PycharmProjects/YOLO/src/Kennzeichenerkennung_Valentin/Schrifterkennung/img/licence_plate.png')
+result = reader.readtext(thresh)
 
 
 for (bbox, text, prob) in result:
