c77e35adf8f40cd9f4fc2f47972977de796f9b0c
PI860 <paulina.pyczot@gmail.com>
2025-04-01 14:58:43 +0200

    feat: Gesichtserkennung angefangen

---
+++ src/face_recognition/face_recognition_paula/test1.py
@@ -0,0 +1,17 @@
+#Sources:
+#https://github.com/akanametov/yolo-face/tree/dev
+#https://medium.com/@erdibillkaan/object-detection-and-face-recognition-with-yolov8n-356b22bacc48
+#https://github.com/carolinedunn/facial_recognition/tree/main
+
+
+import cv2
+from ultralytics import YOLO
+import face_recognition
+
+#face_recognition needs to be installed with
+#pip install cmake
+#pip install dlib
+#pip install face_recognition
+
+import numpy as np
+
