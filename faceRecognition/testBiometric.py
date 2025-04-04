4ccdeaafd550115544698b30a252ba790cb0faea
Chantal <deusch01@hs-ulm.de>
2025-04-04 15:31:22 +0200

    added face contour

--- faceRecognition/testBiometric.py
+++ faceRecognition/testBiometric.py
@@ -19,20 +19,25 @@ mp_drawing = mp.solutions.drawing_utils
 
 # based on Mediapipe documentation -> https://storage.googleapis.com/mediapipe-assets/documentation/mediapipe_face_landmark_fullsize.png
 # Landmark indices for left and right eyes
-LEFT_EYE_IDX = [33, 160, 158, 133, 153, 144]
-RIGHT_EYE_IDX = [362, 385, 387, 263, 373, 380]
+LEFT_EYE_IDX = [33, 160, 158, 133, 153, 144, 33]
+RIGHT_EYE_IDX = [362, 385, 387, 263, 373, 380, 362]
 
 # Landmark indices for left and right eyebrows
-OUTER_LEFT_EYEBROW_IDX = [70, 63, 105, 66, 107]       # Outer left eyebrow arch
-OUTER_RIGHT_EYEBROW_IDX = [336, 296, 334, 293, 300]   # Outer right eyebrow arch
-INNER_LEFT_EYEBROW_IDX = [46, 53, 52, 65, 55]         # Inner left eyebrow arch
-INNER_RIGHT_EYEBROW_IDX = [285, 295, 282, 283, 276]   # Inner right eyebrow arch
+OUTER_LEFT_EYEBROW_IDX = [70, 63, 105, 66, 107]
+OUTER_RIGHT_EYEBROW_IDX = [336, 296, 334, 293, 300]
+INNER_LEFT_EYEBROW_IDX = [46, 53, 52, 65, 55]
+INNER_RIGHT_EYEBROW_IDX = [285, 295, 282, 283, 276]
 
 # Landmark indices for lips
 UPPER_OUTER_LIP_IDX = [61, 185, 40, 39, 37, 0, 267, 269, 270, 409, 291]
 UPPER_INNER_LIP_IDX = [78, 191, 80, 81, 82, 13, 312, 311, 310, 415, 308]
 LOWER_OUTER_LIP_IDX = [61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291]
 LOWER_INNER_LIP_IDX = [78, 95, 88, 178, 87, 14, 317, 402, 318, 324, 308]
+
+# Landmark indices for face contour
+FACE_CONTOUR_IDX = [10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361,
+                    288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149,
+                    150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103, 67, 109, 10]
 
 
 def draw_feature(frame, landmark_list, indices, width, height, color):
@@ -70,6 +75,8 @@ while cap.isOpened():
             draw_feature(frame, face_landmarks, UPPER_INNER_LIP_IDX, width, height, (128, 0, 255))  # Violett
             draw_feature(frame, face_landmarks, LOWER_OUTER_LIP_IDX, width, height, (128, 0, 255))  # Violett
             draw_feature(frame, face_landmarks, LOWER_INNER_LIP_IDX, width, height, (128, 0, 255))  # Violett
+            draw_feature(frame, face_landmarks, FACE_CONTOUR_IDX, width, height, (255, 165, 0))  # Orange
+
 
     cv2.imshow("Biometric Face Recognition", frame)
     if cv2.waitKey(5) & 0xFF == 27:  # ESC
