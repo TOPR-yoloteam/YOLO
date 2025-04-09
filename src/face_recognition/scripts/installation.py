43427f92ac94ad9f5f40252813d8c6913bd1d9ff
PI860 <paulina.pyczot@gmail.com>
2025-04-09 16:16:18 +0200

    x

--- src/face_recognition/scripts/installation.py
+++ src/face_recognition/scripts/installation.py
@@ -2,15 +2,24 @@ import os
 
 # List of required libraries for face recognition and license plate recognition
 libraries = [
-    "face_recognition",  # For face recognition
     "cv2",  # For image processing and real-time recognition
     "numpy",  # Mathematical operations on images/arrays
-    "pillow",  # Image processing
+    "ultralytics" #import YOLO
     "matplotlib",  # For visualizing results
     "easyocr",  # Optical character recognition (OCR) for license plates
     "mediapipe",  # Image segmentation and processing
+    "face_recognition",  # YOLO lib for face recognition
+    "time"
+    "pygame"
+    "threading" #import Thread
+    "os"
+    "queue" #import Queue
 
 ]
+#mediapipe 0.10.21 requires numpy<2, but you have numpy 2.2.4 which is incompatible.
+#ultralytics 8.3.100 requires numpy<=2.1.1,>=1.23.0, but you have numpy 2.2.4 which is incompatible.
+#numpy==1.26.4
+#python==3.12.9
 
 # Install each library
 print("Starting installation of required libraries...")
