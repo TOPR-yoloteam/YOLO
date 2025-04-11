8f656a2d667554d356181252770d3a4035bb75dc
Valentin.Talmon <talmva01@thu.de>
2025-04-11 12:38:39 +0200

    different threshholds v 1.0.0

--- src/license_plate_recognition/scripts/final/ocr_licence_plate.py
+++ src/license_plate_recognition/scripts/final/ocr_licence_plate.py
@@ -15,7 +15,9 @@ def process_image(image):
     # Convert to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Apply binary thresholding
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    #thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    #thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     return thresh
 
 
